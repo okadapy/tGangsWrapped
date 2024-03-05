47d1d09a6d531204680b05c0fb84bd9892fab4d1
okadapy <ozzajilon@gmail.com>
2024-03-05 17:41:09 +0300

    ready

--- run_server.py
+++ run_server.py
@@ -2,9 +2,4 @@ import uvicorn
 from app.server import app as server
 import asyncio
 
-async def start_server():
-    print("starting server")
-    uvicorn.run(server, port=90, host="0.0.0.0", reload=True)
-
-
-uvicorn.run(server)
+uvicorn.run(server, port=8000, host="0.0.0.0")
