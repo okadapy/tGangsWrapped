fc8dd0641e329097af263ccf8fbfbf630b577961
okadapy <ozzajilon@gmail.com>
2024-03-05 17:28:37 +0300

    fixed some stuff

--- app/server.py
+++ app/server.py
@@ -48,6 +48,12 @@ class Game:
                 if self.grid[n_y][n_x] != player and not self.grid[n_y][n_x]["armor"]:
                     self.grid[n_y][n_x]["player"] = None
 
+    def armor(self, x, y, player):
+        for n_y in range(y - 1, y + 1, 1):
+            for n_x in range(x - 1, x + 1, 1):
+                if self.grid[n_y][n_x]["player"] == player and not self.grid[n_y][n_x]["armor"]:
+                    self.grid[n_y][n_x]["armor"] = True
+
     def render(self, player):
         matrix = []
         for row in self.grid:
@@ -134,6 +140,9 @@ async def websocket_handler(websocket: WebSocket):
                     
                 elif data["type"] == "bomb":
                     game.bomb(data["x"], data["y"], players[websocket])
+                
+                elif data["type"] == "armor":
+                    game.armor(data["x"], data["y"])
 
                 if players[websocket].boost:
                     await manager.send_can_place(websocket, 0)
--- run_server.py
+++ run_server.py
@@ -2,14 +2,9 @@ import uvicorn
 from app.server import app as server
 import asyncio
 
-# async def start_bot():
-#     print("Starting bot")
-#     await dp.start_polling(bot)
-
-
 async def start_server():
     print("starting server")
-    uvicorn.run(server)
+    uvicorn.run(server, port=90, host="0.0.0.0", reload=True)
 
 
 uvicorn.run(server)
